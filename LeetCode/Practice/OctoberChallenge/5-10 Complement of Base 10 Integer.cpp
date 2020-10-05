fd223fa9c10319c37272e10e5b7cd224b53b1a90
Aditi Singh <65867928+soamOne@users.noreply.github.com>
2020-10-05 15:22:20 +0530

    Create 5-10 Complement of Base 10 Integer.cpp

---
+++ LeetCode/Practice/OctoberChallenge/5-10 Complement of Base 10 Integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int bitwiseComplement(int N) {
+        vector<int> binN;//vector<int> binN(32); 
+        if(N==0) return 1;
+        while(N!=0){
+            binN.push_back(N%2);
+            N=N/2;
+        }
+        reverse(binN.begin(),binN.end());
+        int res=0;int j=0;
+        for(int i=binN.size()-1;i>=0;i--){
+            //cout<<binN[i];
+            if (binN[i]==0) res+=pow(2,j)*1;
+            j++;
+        }return res;
+        
+    }
+};
